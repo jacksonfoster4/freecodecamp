4377e1786096cca8e9897feff79c4bb8177c60cc
Jackson Foster <jacksonfoster444@gmail.com>
2018-06-06 18:45:11 -0500

    small change

--- basic_algorithms/falsyBouncer.js
+++ basic_algorithms/falsyBouncer.js
@@ -3,13 +3,12 @@ Remove all falsy values from an array.
 */
 
 function bouncer(arr) {
-  let filtered = arr.reduce((final, el) => {
+  return arr.reduce((final, el) => {
     if (Boolean(el)){
       final.push(el)
     }
     return final
   }, [])
-  return filtered;
 }
 
 bouncer([7, "ate", "", false, 9]);
